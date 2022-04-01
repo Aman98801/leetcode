ce7d2b92acd7161ff3860d7e7f889e1b5aa9dd29
Aman.Prajapat <78609887+Aman98801@users.noreply.github.com>
2022-04-01 23:45:28 +0530

    Time: 182 ms (5.48%), Space: 49.8 MB (82.54%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,5 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
+     
+    /*
       int start = 0;
       int end = s.length-1;
         
@@ -10,9 +12,32 @@ class Solution {
             start++;
             end--;
         }
-//         for(char i : s){
-//             System.out.println(i);
-//         }
+         for(char i : s){
+            System.out.println(i);
+        }
+        */
+        
+        // second logic 
+        
+        String s1 = "";
+        for(int i=0;i<s.length;i++){
+            String s2 = Character.toString(s[i]);
+            s1+=s2;
+        }
+        StringBuilder sb = new StringBuilder(s1);
+        StringBuilder sb1 = sb.reverse();
+        String str[] = sb1.toString().split("");
+        
+        String s3 = "";
+        for(int i=0;i<s.length;i++){
+           s3+=str[i];
+        }
+        
+        char[] result = s3.toCharArray();
+        for(int i=0;i<s.length;i++){
+           s[i] = result[i];
+        }
+        
         
     }
     

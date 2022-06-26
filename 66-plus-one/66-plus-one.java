e0b1a60ab3d36a400005f377ab556c0c13a31612
Aman.Prajapat <78609887+Aman98801@users.noreply.github.com>
2022-06-26 11:02:02 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (33.70%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-          int carry = 1;
+    int carry = 1;
     for (int i = digits.length-1; i>= 0; i--) {
         digits[i] += carry;
         if (digits[i] <= 9) // early return 

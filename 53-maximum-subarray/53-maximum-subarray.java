df87a992215a9e9f7c43e820ed01a1ad128d42f0
Aman.Prajapat <78609887+Aman98801@users.noreply.github.com>
2022-09-03 15:30:02 +0530

    Time: 2 ms (74.44%), Space: 73.4 MB (69.27%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -2,15 +2,22 @@ import java.util.Arrays;
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = nums[0];
-        int sum = 0;
+        int globalMax = Integer.MIN_VALUE;   // globalMax = -2147483648
+        // System.out.println(globalMax);
         
-        for(int i = 0; i < nums.length; i++){
-            sum = sum + nums[i];
-            
-            if(sum > max) max = sum;
-            if(sum < 0 ) sum = 0;
-        }
-        return max;
+        int localMax = 0;
+        
+        for (int i =0; i < nums.length; i++){
+            if (localMax<0){
+                localMax =  nums[i];   // localMax = -2(i=0), 1(i=1),4(i=3),
+            }else{
+                localMax = localMax+nums[i]; // localMax = -2(i=2),3(i=4),5(i=5),6(i=6),1(i=7),5(i=8)
+            }
+        // localMax= Math.max(localMax+nums[i],nums[i]);
+            globalMax = Math.max(globalMax,localMax);   // -2(i=0),1(i=1),1(i=2),4(i=3),4(i=4),5(i=5),6(i=6)
+                                                        //  6(i=7),6(i=8)
+            }
+
+        return globalMax;
     }
 }
